e65019f5843a548309ee1eb8a51fb64a2a1d5b28
Émile Gauvreau <gauvreau.emile@outlook.com>
2017-08-31 14:30:07 -0400

    Ajout de la fonction lol

--- test_git/Program.cs
+++ test_git/Program.cs
@@ -10,6 +10,23 @@ namespace test_git
     {
         static void Main(string[] args)
         {
+            cheval:
+            Console.WriteLine("Hey! Qu'a tu à me dire aujourd'hui?");
+            String txt = Console.ReadLine();
+            Console.WriteLine("Tu viens d'écrire: " + txt);
+            if (txt == "lol")
+            {
+                Console.WriteLine("LOL!");
+                Console.WriteLine("MDR");
+                Console.WriteLine("PTDR");
+                Console.WriteLine("XD");
+                Console.WriteLine();
+            }
+            if (txt != "stop")
+            {
+                goto cheval;
+            }
+
         }
     }
 }
